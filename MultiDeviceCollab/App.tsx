60e178c5a1c6879aa90bbe72664cd4da2e445990
Soline HAYES <soline.hayes@telecom-paristech.fr>
2020-09-14 13:46:10 +0200

    feat: prettier
    Install

--- MultiDeviceCollab/App.tsx
+++ MultiDeviceCollab/App.tsx
@@ -1,16 +1,12 @@
 import React, {FunctionComponent} from 'react';
-import {
-  SafeAreaView,  
-  Text,
-} from 'react-native';
-
-
+import {SafeAreaView, Text} from 'react-native';
 
 const App: FunctionComponent = () => {
   return (
-    <SafeAreaView><Text>Init</Text></SafeAreaView>
+    <SafeAreaView>
+      <Text>Init</Text>
+    </SafeAreaView>
   );
 };
-
 
 export default App;
