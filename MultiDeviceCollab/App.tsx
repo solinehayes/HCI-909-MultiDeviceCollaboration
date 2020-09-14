5785354c20cbb0965d2f1c755594e5b1edc188a5
Soline HAYES <soline.hayes@telecom-paristech.fr>
2020-09-14 13:39:23 +0200

    feat: tS
    Add typescript

---
+++ MultiDeviceCollab/App.tsx
@@ -0,0 +1,16 @@
+import React, {FunctionComponent} from 'react';
+import {
+  SafeAreaView,  
+  Text,
+} from 'react-native';
+
+
+
+const App: FunctionComponent = () => {
+  return (
+    <SafeAreaView><Text>Init</Text></SafeAreaView>
+  );
+};
+
+
+export default App;
