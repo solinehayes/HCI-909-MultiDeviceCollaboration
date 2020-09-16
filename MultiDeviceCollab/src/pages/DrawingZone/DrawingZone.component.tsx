a750e276e1ab91177ef82a4bdeec20448ba47c4e
Soline HAYES <soline.hayes@telecom-paristech.fr>
2020-09-16 16:38:18 +0200

    adding different buttons

--- MultiDeviceCollab/src/pages/DrawingZone/DrawingZone.component.tsx
+++ MultiDeviceCollab/src/pages/DrawingZone/DrawingZone.component.tsx
@@ -1,23 +1,35 @@
 import React, {FunctionComponent} from 'react';
-import {Text, SafeAreaView, ViewStyle, StyleSheet} from 'react-native';
+import {View, SafeAreaView, ViewStyle, StyleSheet} from 'react-native';
 import {FloatingButton} from '../../components/FloatingButton/FloatingButton.component';
 import {theme} from '../../../theme';
 
 interface Styles {
   container: ViewStyle;
+  bottomButtonContainer: ViewStyle;
 }
 
 const styles = StyleSheet.create<Styles>({
   container: {
     flex: 1,
     backgroundColor: theme.colors.white,
+    justifyContent: 'space-between',
+  },
+  bottomButtonContainer: {
+    alignItems: 'flex-end',
   },
 });
 
 export const DrawingZone: FunctionComponent = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <FloatingButton iconName="rocket" />
+      <View>
+        <FloatingButton iconName="file-o" />
+        <FloatingButton iconName="undo" />
+        <FloatingButton iconName="pencil" />
+      </View>
+      <View style={styles.bottomButtonContainer}>
+        <FloatingButton iconName="bluetooth-b" />
+      </View>
     </SafeAreaView>
   );
 };
