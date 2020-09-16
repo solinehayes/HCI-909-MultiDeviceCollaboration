1ca556d162c90cadf14f0aaa751c8cd583c8c7b0
Soline HAYES <soline.hayes@telecom-paristech.fr>
2020-09-16 16:12:07 +0200

    design new component

--- MultiDeviceCollab/src/components/FloatingButton/FloatingButton.component.tsx
+++ MultiDeviceCollab/src/components/FloatingButton/FloatingButton.component.tsx
@@ -1,6 +1,35 @@
 import React, {FunctionComponent} from 'react';
-import {View} from 'react-native';
+import {TouchableOpacity, StyleSheet, ViewStyle} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export const FloatingButton: FunctionComponent = () => {
-  return <View />;
+interface Props {
+  iconName: string;
+}
+interface Styles {
+  container: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ECECEC',
+    padding: 8,
+    margin: 8,
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+  },
+});
+
+export const FloatingButton: FunctionComponent<Props> = ({iconName}) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        console.log('test');
+      }}>
+      <Icon name={iconName} size={20} />
+    </TouchableOpacity>
+  );
 };
