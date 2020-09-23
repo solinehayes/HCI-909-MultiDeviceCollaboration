eb9e98573a075eedef1deb94ae00128f442c823b
ebnnl <bonnail.elise@laposte.net>
2020-09-23 20:13:47 +0200

    Add swipe gesture detector

--- MultiDeviceCollab/src/App.tsx
+++ MultiDeviceCollab/src/App.tsx
@@ -1,26 +1,20 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent}from 'react';
 import {DrawingZone} from './pages/DrawingZone/DrawingZone.component';
 import {SwipeConfiguration} from './pages/SwipeConfiguration/SwipeConfiguration.component';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 const App: FunctionComponent = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="DrawingZone" component={DrawingZone} />
-          <Stack.Screen
-            name="SwipeConfiguration"
-            component={SwipeConfiguration}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="DrawingZone" component={DrawingZone} />
+        <Stack.Screen name="SwipeConfiguration" component={SwipeConfiguration} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
