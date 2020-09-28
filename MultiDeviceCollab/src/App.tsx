f0250e00703db619e3ad1084aecb40eb387668fb
Soline HAYES <soline.hayes@telecom-paristech.fr>
2020-09-28 21:56:19 +0200

    type navigation

--- MultiDeviceCollab/src/App.tsx
+++ MultiDeviceCollab/src/App.tsx
@@ -4,15 +4,27 @@ import {SwipeConfiguration} from './pages/SwipeConfiguration/SwipeConfiguration.
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-const Stack = createStackNavigator();
+
+export type RootStackParamList = {
+  DrawingZone: undefined;
+  SwipeConfiguration: undefined;
+};
+export enum RootNavigatorRouteNames {
+  SwipeConfiguration = 'SwipeConfiguration',
+  DrawingZone = 'DrawingZone',
+}
+const Stack = createStackNavigator<RootStackParamList>();
 
 const App: FunctionComponent = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="DrawingZone" component={DrawingZone} />
         <Stack.Screen
-          name="SwipeConfiguration"
+          name={RootNavigatorRouteNames.DrawingZone}
+          component={DrawingZone}
+        />
+        <Stack.Screen
+          name={RootNavigatorRouteNames.SwipeConfiguration}
           component={SwipeConfiguration}
         />
       </Stack.Navigator>
